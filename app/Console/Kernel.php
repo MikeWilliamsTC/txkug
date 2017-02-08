cb6c3cdbe9aee9272add3c23c6be37507e01cc0d
Randy Rankin <rrrankin@gmail.com>
2017-02-08 13:01:52 -0600

    Added Social Logins Seeder

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -46,8 +46,7 @@ class Kernel extends ConsoleKernel
                     'avatar_192' => $response['user']['profile']['image_192'],
                 ]);
             });
-        })->everyMinute();
-//            ->twiceDaily(7, 13);
+        })->twiceDaily(7, 13);
     }
 
     /**
