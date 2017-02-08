2281a654954b77ff4c5258af893c3b9b740927c4
Randy Rankin <rrrankin@gmail.com>
2017-02-08 09:28:17 -0600

    Navigation Tweaks

--- resources/views/layouts/welcome-nav.blade.php
+++ resources/views/layouts/welcome-nav.blade.php
@@ -5,7 +5,6 @@
         </button>
         <a class="navbar-brand" href="/">
             TXKUG
-            {{--<img src="https://mdbootstrap.com/img/logo/mdb-transparent-sm-shadows.png" class="d-inline-block align-top" alt="MDBootstrap">--}}
         </a>
 
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -20,35 +19,11 @@
             </ul>
 
             <ul class="navbar-nav">
-                @if (Auth::guest())
+                @if ( ! Auth::guest())
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('social.redirect', ['provider' => 'slack']) }}">Slack Login</a>
+                        <a class="nav-link" href="{{ route('user.home') }}">{{ Auth::user()->name }}</a>
                     </li>
-                @else
-                    @if(Auth::user()->hasRole('administrator'))
-
-                    <li class="nav-item dropdown btn-group">
-                        <a class="nav-link dropdown-toggle" id="adminDropMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Admin</a>
-                        <div class="dropdown-menu" aria-labelledby="adminDropMenu">
-                            <a class="dropdown-item" href="/admin">Dashboard</a>
-                            <a class="dropdown-item" href="/admin/venues">Venues</a>
-                            <a class="dropdown-item" href="/admin/events">Events</a>
-                            <a class="dropdown-item" href="/admin/users">Users</a>
-                        </div>
-                    </li>
-
-                    @endif
-
-                    <li class="nav-item dropdown btn-group">
-                        <a class="nav-link dropdown-toggle" id="userDropMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
-                        <div class="dropdown-menu" aria-labelledby="userDropMenu">
-                            <a class="dropdown-item" href="{{ url('/user/meetings') }}">My Events</a>
-                            <a class="dropdown-item" href="{{ url('/logout') }}">Logout</a>
-                        </div>
-                    </li>
-
                 @endif
-
             </ul>
         </div>
     </div>
