660ad20b6856d9eb02972b35ce338befaa39a5a0
Randy Rankin <rrrankin@gmail.com>
2017-02-24 08:31:47 -0600

    Integrate data and time pickers

--- resources/views/admin/events/edit.blade.php
+++ resources/views/admin/events/edit.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 
 @section('header_scripts')
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.css" rel="stylesheet">
+    <link href="{{ asset('assets/plugins/summernote/summernote.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/plugins/jquery-timepicker/jquery.timepicker.css') }}" rel="stylesheet" />
 @endsection
 
 @section('content')
@@ -40,12 +43,43 @@
 @stop
 
 @section('footer_scripts')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.js"></script>
+    <script src="{{ asset('assets/plugins/summernote/summernote.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/plugins/jquery-timepicker/jquery.timepicker.min.js') }}" type="text/javascript"></script>
     <script>
         $(document).ready(function() {
             $('#event-description').summernote({
                 height: 225
             });
+
+            $('#event-date').datepicker({
+                format: 'yyyy-mm-dd',
+                todayHighlight: true,
+                autoclose: true
+            });
+
+            $('#starts-at').timepicker({
+                minTime: '08:00',
+                maxTime: '22:00',
+                step: 15,
+                timeFormat: 'H:i'
+            });
+
+            $('#stops-at').timepicker({
+                minTime: '08:00',
+                maxTime: '22:00',
+                step: 15,
+                timeFormat: 'H:i'
+            });
+
+            $(document).on('click', '.browse', function(){
+                var file = $(this).parent().parent().parent().find('.file');
+                file.trigger('click');
+            });
+            $(document).on('change', '.file', function(){
+                $(this).parent().find('.form-control').val($(this).val().replace(/C:\\fakepath\\/i, ''));
+            });
+
         });
     </script>
 
--- resources/views/admin/events/form.blade.php
+++ resources/views/admin/events/form.blade.php
@@ -1,12 +1,22 @@
+
+@if ( isset($event->event_image) )
+    <div class="form-group">
+        {!! Form::label('event_image', 'Image Preview', ['class' => 'col-sm-2 control-label']) !!}
+        <div class="col-sm-6">
+            <img class="img-thumbnail" src="/storage/events/{{ $event->event_image }}" alt="{{ $event->event_title }}">
+        </div>
+    </div>
+@endif
+
 <div class="form-group">
     {!! Form::label('event_image', 'Event Image', ['class' => 'col-sm-2 control-label']) !!}
-    <div class="col-sm-6">
-        {{ Form::file('event_image_file') }} <br />
-        @if ( isset($event->event_image) )
-            <img class="img-thumbnail" src="/storage/events/{{ $event->event_image }}" alt="{{ $event->event_title }}">
-        @endif
+    <div class="input-group col-sm-6">
+        <span class="input-group-btn margin">
+            <button class="browse btn btn-primary" style="margin-left: 15px;" type="button"> Browse</button>
+        </span>
+        <input type="text" class="form-control" style="background: transparent;" disabled placeholder="Upload Image">
     </div>
-
+    <input type="file" name="event_image_file" class="file col-sm-2 control-label" style="visibility: hidden; position: absolute;">
 </div>
 
 <div class="form-group">
@@ -24,7 +34,7 @@
 </div>
 
 <div class="form-group">
-    {!! Form::label('venue_id', 'Venue', ['class' => 'col-sm-2 control-label']) !!}
+    {!! Form::label('venue_id', 'Event Venue', ['class' => 'col-sm-2 control-label']) !!}
     <div class="col-sm-10">
         {!! Form::select('venue_id', $venues, null, ['class' => 'form-control', 'placeholder' => 'Select a Venue']) !!}
     </div>
@@ -33,18 +43,18 @@
 <div class="form-group">
     {!! Form::label('event_date', 'Event Date', ['class' => 'col-sm-2 control-label']) !!}
     <div class="col-sm-3">
-        {!! Form::text('event_date', null, ['class' => 'form-control']) !!}
+        {!! Form::text('event_date', null, ['id' => 'event-date', 'class' => 'form-control']) !!}
     </div>
 </div>
 
 <div class="form-group">
     {!! Form::label('starts_at', 'Start Time', ['class' => 'col-sm-2 control-label']) !!}
     <div class="col-sm-3">
-        {!! Form::text('starts_at', null, ['class' => 'form-control']) !!}
+        {!! Form::text('starts_at', null, ['id' => 'starts-at' ,'class' => 'form-control time']) !!}
     </div>
     {!! Form::label('stops_at', 'Stop Time', ['class' => 'col-sm-2 control-label']) !!}
     <div class="col-sm-3">
-        {!! Form::text('stops_at', null, ['class' => 'form-control']) !!}
+        {!! Form::text('stops_at', null, ['id' => 'stops-at', 'class' => 'form-control time']) !!}
     </div>
 </div>
 
