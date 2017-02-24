c3447179b4ad42312beae89414b476a0b718c847
Randy Rankin <rrrankin@gmail.com>
2017-02-24 14:35:32 -0600

    Add Social Icons to footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,40 +1,51 @@
-<!-- BEGIN: LAYOUT/FOOTERS/FOOTER-2 -->
+<!-- BEGIN: LAYOUT/FOOTERS/FOOTER-7 -->
 <a name="footer"></a>
-<footer class="c-layout-footer c-layout-footer-3 c-bg-dark">
+<footer class="c-layout-footer c-layout-footer-7">
+    <div class="container">
+        <div class="c-prefooter">
 
-    <div class="c-prefooter">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="c-container c-first">
-                        <div class="c-content-title-1">
-                            <h3 class="c-font-uppercase c-font-bold c-font-white">TXK
+            {{--<div class="c-line"></div>--}}
+            <div class="c-head">
+                <div class="row">
+                    <div class="col-md-7">
+                        <div class="c-content-title-1 c-title-md">
+                            <h3 class="c-title c-font-uppercase c-font-bold">About TXK
                                 <span class="c-theme-font">UG</span>
                             </h3>
                             <div class="c-line-left hide"></div>
-                            <p class="c-text">
-                                TXKUG is a
-                                community of information technology professionals who come together in the spirit
-                                of learning and fellowship.
-                            </p>
+                        </div>
+                        <p class="c-text c-font-16 c-font-regular">
+
+                            Tolerare unus ducunt ad brevis buxum. Est alter buxum, cesaris. Eheu, lura!
+                            Racanas crescere in emeritis oenipons! Ubi est rusticus repressor? Lixa grandis clabulare est.
+                            Eposs tolerare, tanquam fatalis.
+
+                        </p>
+                    </div>
+                    <div class="col-md-5">
+                        <div class="c-right">
+                            <div class="socicon">
+                                <a href="https://www.facebook.com/txkug/?fref=ts" target="_blank" class="socicon-btn socicon-btn-circle socicon-solid c-bg-dark-1 c-font-dark-2 c-font-white-hover socicon-facebook tooltips" data-original-title="Facebook" data-container="body"></a>
+                                <a href="https://twitter.com/Txkug" target="_blank" class="socicon-btn socicon-btn-circle socicon-solid c-bg-dark-1  c-font-dark-2 c-font-white-hover socicon-twitter tooltips" data-original-title="Twitter" data-container="body"></a>
+                                <a href="https://la.linkedin.com/in/texarkana-area-it-users-group-471730130?trk=pub-pbmap" target="_blank" class="socicon-btn socicon-btn-circle socicon-solid c-bg-dark-1 c-font-dark-2 c-font-white-hover socicon-linkedin tooltips" data-original-title="LinedIn" data-container="body"></a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
-
-    <div class="c-postfooter">
+    <div class="c-postfooter c-bg-dark-2">
         <div class="container">
             <div class="row">
                 <div class="col-md-6 col-sm-12 c-col">
-                    <p class="c-copyright c-font-grey"> Copyright <?php echo date('Y'); ?> &copy; {{ env('ORG_NAME') }}
-                        <span class="c-font-grey-3">All Rights Reserved.</span>
+                    <p class="c-copyright c-font-grey">&copy; <?php echo date('Y'); ?> Texarkana Area Information Technology Users Group
+                        <span class="c-font-grey-3"> All Rights Reserved.</span>
                     </p>
                 </div>
             </div>
         </div>
     </div>
-
 </footer>
-<!-- END: LAYOUT/FOOTERS/FOOTER-2 -->
+<!-- END: LAYOUT/FOOTERS/FOOTER-7 -->
